061f107731b84511c18232d73e90adb26bde3571
rhrastic <ryanhrastich@yahoo.com>
2023-03-12 22:48:32 -0500

    Add files via upload

---
+++ testEnvironment/homepage.js
@@ -0,0 +1,8 @@
+function playTivia(){
+    document.getElementById("homepage").classList.add("hide");
+    document.getElementById("trivia").classList.remove("hide");
+}
+function playDsweeper(){
+    document.getElementById("homepage").classList.add("hide");
+    document.getElementById("mine").classList.remove("hide");
+}
