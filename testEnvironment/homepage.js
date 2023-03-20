4e67798f1aa5f2ec066fb4036c1caa94aa223841
rhrastic <ryanhrastich@yahoo.com>
2023-03-20 11:42:04 -0500

    Delete testEnvironment directory

--- testEnvironment/homepage.js
+++
@@ -1,8 +0,0 @@
-function playTivia(){
-    document.getElementById("homepage").classList.add("hide");
-    document.getElementById("trivia").classList.remove("hide");
-}
-function playDsweeper(){
-    document.getElementById("homepage").classList.add("hide");
-    document.getElementById("mine").classList.remove("hide");
-}
