2f954cce4d24d9f5c994bfe5aa50f693b2256cc0
rhrastic <ryanhrastich@yahoo.com>
2023-03-23 12:45:22 -0500

    Update phishingFrenzy.js

--- phishingFrenzy/phishingFrenzy.js
+++ phishingFrenzy/phishingFrenzy.js
@@ -14,6 +14,9 @@ var medium = 2000;
 var hard = 1000;
 var difficulty;
 var gameOverMessage;
+const win = "win"
+const lose = "lose"
+const reload = "reload"
 
 var startTime;
 
@@ -26,60 +29,186 @@ reportButton = document.getElementById("report_button");
 easyButton = document.getElementById("easy");
 mediumButton = document.getElementById("medium");
 hardButton = document.getElementById("hard");
-response = document.getElementById("response");
 playGame = document.getElementById('inGame')
 difficultySelector = document.getElementById('difficultySelector')
 playerMessage = document.getElementById('playerMessage')
 
 easyButton.addEventListener('click', () => {
-    startTime = Math.floor(Date.now() / 1000);
     difficulty = easy;
-    point = difficulty;
-    difficultySelector.classList.add('hide');
-    playGame.classList.remove('hide');
     startGame();
 });
 
 mediumButton.addEventListener('click', () => {
-    startTime = Math.floor(Date.now() / 1000);
     difficulty = medium;
-    point = difficulty;
-    difficultySelector.classList.add('hide');
-    playGame.classList.remove('hide');
     startGame();
 });
 
 hardButton.addEventListener('click', () => {
-    startTime = Math.floor(Date.now() / 1000);
     difficulty = hard;
-    point = difficulty;
-    difficultySelector.classList.add('hide');
-    playGame.classList.remove('hide');
     startGame();
 });
 
 function startGame() {
+    difficultySelector.classList.add('hide');
+    playGame.classList.remove('hide');
+    point = difficulty;
     score = 0;
     time = 0;
-    scoreDisplay;
-    timeDisplay;
-    emailDisplay;
-    reportButton;
     endGame = false;
-    emailInterval;
-    gameOverMessage = "";
+    emailInterval = null;
+    email = null;
+    startTime = Math.floor(Date.now() / 1000);
     phishingFrenzy()
 }
 
 function phishingFrenzy() {
 
-    updateGame();
+    updateGame(reload);
+    function updateGame(x) {
+        clearInterval(emailInterval);
+        clearInterval(pointInterval);
+        if (x === reload) {
+            generateEmail();
+            console.log(emails);
+            emailDisplay.innerText = emails.text
+            emailInterval = setInterval(function () {
+                checkEmail(false);
+            }, difficulty);
+            point = difficulty;
+            pointInterval = setInterval(function () {
+                lowerScore();
+            }, 100);
+        }
+        else if (x === lose) {
+            gameOver();
+        }
+        else {
+            gameWin();
+        }
+    }
 
-    score = 0;
+    reportButton.addEventListener('click', reportButtonClick);
+    function reportButtonClick() { checkEmail(true); }
 
+    function checkEmail(reported) {
+        if (reported) {
+            if (emails.phishing) {
+                score = score + point;
+                updateGame(reload);
+            }
+            else {
+                gameOverMessage = "That was a real Email"
+                endGame = true;
+                updateGame(lose);
+            }
+        }
+        else {
+            if (emails.phishing) {
+                gameOverMessage = "You have been phished!!!";
+                updateGame(lose);
+            }
+            else {
+                score = score + 1000;
+                updateGame(reload);
+            }
+        }
+    }
+    function lowerScore() {
+        if (point <= 0) {
+            clearInterval(pointInterval);
+        }
+        else {
+            point = point - 100;
+        }
+    }
+
+    function gameWin() {
+        restartGame();
+        playerMessage.innerText = `Congratulations! You've won! Final Score: ${score}`;
+    }
+
+    function gameOver() {
+        restartGame();
+        playerMessage.innerText = gameOverMessage;
+    }
+
+    function restartGame() {
+        clearInterval(timerInterval);
+        playGame.classList.add('hide');
+        difficultySelector.classList.remove('hide')
+        reportButton.removeEventListener('click', reportButtonClick);
+    }
+
+    let timerInterval = setInterval(updateScoreAndTime, 1000);
+    function updateScoreAndTime() {
+        var elapsedTime = Math.floor(Date.now() / 1000) - startTime;
+        var timeRemaining = gameTime - elapsedTime;
+        if (timeRemaining <= 0) {
+            endGame = true;
+            updateGame(win);
+        }
+        scoreDisplay.innerText = score;
+        timeDisplay.innerText = timeRemaining;
+    }
     function generateEmail() {
 
         const emailTexts = [
+            {
+                phishing: false,
+                text: "Hello [Your Name], I hope this email finds you well. We are excited to announce a new product that we think you will love. Check it out at the link below.",
+            },
+            {
+                phishing: true,
+                text: "Important notice: Your Apple ID has been suspended due to suspicious activity. Click this link to restore your account and prevent further damage.",
+            },
+            {
+                phishing: false,
+                text: "Dear [Recipient Name], thank you for your recent purchase. Your order has been shipped and should arrive within 2-3 business days. You can track your package at the link below.",
+            },
+            {
+                phishing: true,
+                text: "URGENT: Your Google account has been hacked! Click this link to reset your password and secure your account immediately.",
+            },
+            {
+                phishing: false,
+                text: "Hi [Recipient Name], I wanted to touch base and see how things are going with the project. Let me know if there's anything I can do to help.",
+            },
+            {
+                phishing: true,
+                text: "Security alert: Your Facebook account has been compromised! Click this link to verify your account information and prevent unauthorized access.",
+            },
+            {
+                phishing: false,
+                text: "Hello [Your Name], I hope this email finds you well. I wanted to share an interesting article I came across that I thought you might enjoy. Check it out at the link below.",
+            },
+            {
+                phishing: true,
+                text: "Attention: Your Microsoft account has been breached! Click this link to change your password and protect your sensitive information.",
+            },
+            {
+                phishing: false,
+                text: "Good morning, I wanted to follow up on the job application you submitted. Can you please provide your availability for an interview?"
+            },
+            {
+                phishing: true,
+                text: "Your Facebook account has been flagged for suspicious activity! Click this link to secure your account before it's too late."
+            },
+            {
+                phishing: false,
+                text: "Dear [Name], I hope this email finds you well. I wanted to touch base with you about the upcoming conference. Are you planning to attend?"
+            },
+            {
+                phishing: true,
+                text: "Your Netflix subscription has been canceled! Click this link to reactivate your account and avoid any additional charges."
+            },
+            {
+                phishing: false,
+                text: "Hi [Name], just wanted to let you know that the team is making great progress on the project. We should be able to meet our deadline without any issues."
+            },
+            {
+                phishing: true,
+                text: "Your Apple ID has been locked for security reasons! Click this link to unlock your account and avoid permanent suspension."
+            },
             {
                 phishing: false,
                 text: "Dear valued customer, we have an exciting offer just for you! Click the link to learn more.",
@@ -115,109 +244,9 @@ function phishingFrenzy() {
         ];
 
         var randomIndex = Math.floor(Math.random() * emailTexts.length)
-
-        const randomText = emailTexts[randomIndex].text;
-        const randomphishing = emailTexts[randomIndex].phishing;
         emails = {
-            text: randomText,
-            phishing: randomphishing,
+            text: emailTexts[randomIndex].text,
+            phishing: emailTexts[randomIndex].phishing,
         };
-
-        console.log(emails);
     }
-
-    reportButton.addEventListener('click', () => {
-
-        console.log("click");
-
-        if (emails != null) {
-            if (emails.phishing) {
-                score = score + point;
-                clearInterval(emailInterval)
-                emailInt();
-                updateGame();
-            }
-            else {
-                gameOverMessage = "That was a real Email"
-                gameOver();
-            }
-        }
-    });
-
-    function updateScoreAndTime() {
-        var elapsedTime = Math.floor(Date.now() / 1000) - startTime;
-        var timeRemaining = gameTime - elapsedTime;
-        if (timeRemaining <= 0) {
-            clearInterval(emailInterval);
-            clearInterval(timerInterval);
-            gameWin();
-        }
-        scoreDisplay.innerText = score;
-        timeDisplay.innerText = timeRemaining;
-    }
-
-    function checkEmail() {
-        if(emails == null){
-            updateGame();
-        }
-        else if(emails.phishing){
-            gameOverMessage = "You have been phished!!!";
-            gameOver();
-        }
-        else{
-            score = score + 1000;
-            updateGame();
-        }    
-    }
-
-    function updateGame() {
-
-        if (endGame == false) {
-            generateEmail();
-            emailDisplay.innerText = emails.text
-            pointInt();
-            point = difficulty;
-        }
-        else {
-            clearInterval(emailInterval);
-            clearInterval(pointInterval);
-        }
-    }
-
-    function gameWin() {
-        clearInterval(emailInterval);
-        clearInterval(pointInterval);
-        clearInterval(timerInterval);
-        restartGame();
-        playerMessage.innerText = `Congratulations! You've won! Final Score: ${score}`;
-    }
-
-    function gameOver() {
-        clearInterval(emailInterval);
-        clearInterval(pointInterval);
-        clearInterval(timerInterval);
-        restartGame();
-        playerMessage.innerText = gameOverMessage;
-    }
-
-    function restartGame() {
-        playGame.classList.add('hide');
-        difficultySelector.classList.remove('hide')
-        console.log("-----------------")
-        
-        endGame = true;
-    }
-
-    let timerInterval = setInterval(updateScoreAndTime, 1000);
-
-    function emailInt() { emailInterval = setInterval(checkEmail, difficulty); }
-    emailInt();
-
-    function pointInt() { pointInterval = setInterval(lowerScore, 100); }
-    pointInt();
-
-    function lowerScore(){
-        point = point - 100;
-    }
-
 }
