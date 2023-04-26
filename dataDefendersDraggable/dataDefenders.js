c0f53f96067731ca724db6ed624e144674c45871
rhrastic <ryanhrastich@yahoo.com>
2023-04-26 11:11:47 -0500

    Add files via upload

--- dataDefendersDraggable/dataDefenders.js
+++ dataDefendersDraggable/dataDefenders.js
@@ -791,65 +791,58 @@ function displayInfoBooklet() {
 }
 function displayddNotes() {
     document.getElementById("ddNotes").classList.toggle("hide");
-}
-
-//Make the DIV element draggable:
-dragElement(document.getElementById("ddNotes"));
-dragElement(document.getElementById("ddInfoBook"));
-function dragElement(elmnt) {
+  }
+  
+  function makeDraggable(elmnt) {
     var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-    if (document.getElementById(`Header${elmnt}`)) {
-        /* if present, the header is where you move the DIV from:*/
-        document.getElementById(`Header${elmnt}`).onmousedown = dragMouseDown;
-    } else {
-        /* otherwise, move the DIV from anywhere inside the DIV:*/
-        elmnt.onmousedown = dragMouseDown;
-    }
-
+  
+    elmnt.querySelector(".ddDraggable").onmousedown = dragMouseDown;
+  
     function dragMouseDown(e) {
-        e = e || window.event;
-        e.preventDefault();
-        // get the mouse cursor position at startup:
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        document.onmouseup = closeDragElement;
-        // call a function whenever the cursor moves:
-        document.onmousemove = elementDrag;
+      e = e || window.event;
+      e.preventDefault();
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      document.onmouseup = closeDragElement;
+      document.onmousemove = elementDrag;
     }
-
+  
     function elementDrag(e) {
-        console.log("Top: ", elmnt.offsetTop);
-        console.log("Left: ", elmnt.offsetLeft);
-        e = e || window.event;
-        e.preventDefault();
-        // calculate the new cursor position:
-        pos1 = pos3 - e.clientX;
-        pos2 = pos4 - e.clientY;
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        // set the element's new position:
-        elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-        elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        if (elmnt.offsetTop < 350) {
-            elmnt.style.top = "350px";
-        } else if (elmnt.offsetTop > window.innerHeight+250) {
-            //elmnt.style.top = "950px";;
-            elmnt.style.top= (window.innerHeight+250)+"px"
-        }
-        if (elmnt.offsetLeft < 0) {
-            elmnt.style.left = "0px";
-        } else if (elmnt.offsetLeft > window.innerWidth+250) {
-            elmnt.style.left = (window.innerWidth+250)+"px"
-        }
+      e = e || window.event;
+      e.preventDefault();
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+      elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+      if (elmnt.offsetTop < 350) {
+        elmnt.style.top = "350px";
+    } else if (elmnt.offsetTop > window.innerHeight+250) {
+        //elmnt.style.top = "950px";;
+        elmnt.style.top= (window.innerHeight+250)+"px"
     }
-
-
+    if (elmnt.offsetLeft < 0) {
+        elmnt.style.left = "0px";
+    } else if (elmnt.offsetLeft > window.innerWidth+100) {
+        elmnt.style.left = (window.innerWidth+100)+"px"
+    }
+    }
+  
     function closeDragElement() {
-        /* stop moving when mouse button is released:*/
-        document.onmouseup = null;
-        document.onmousemove = null;
+      document.onmouseup = null;
+      document.onmousemove = null;
     }
-}
+  }
+  
+  function startDrag() {
+    var notes = document.querySelector("#ddNotes");
+    var notes = document.querySelector("#ddInfoBook");
+    notes.style.zIndex = 10;
+  }
+  
+  makeDraggable(document.getElementById("ddNotes"));
+  makeDraggable(document.getElementById("ddInfoBook"));
 
 var current_page = 1;
 
